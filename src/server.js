cbe20f721b64c422ed985ed57006f46516ff4a04
Dailton Bastos <dailtonbastos@gmail.com>
2020-08-08 17:45:47 -0300

    feat: add routes not found

--- src/server.js
+++ src/server.js
@@ -10,6 +10,12 @@ app.use(express.json());
 app.use(routes);
 app.use(cors());
 
+app.use((req, res, next) => {
+  const error = new Error('Not found!');
+  error.status = 404;
+  return next(error);
+});
+
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({ error: error.message });
