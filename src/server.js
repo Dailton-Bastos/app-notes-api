af8b47356db56882b949ce96b13238c1f6757790
Dailton Bastos <dailtonbastos@gmail.com>
2020-08-10 10:38:06 -0300

    fix: alter order set cors

--- src/server.js
+++ src/server.js
@@ -8,9 +8,9 @@ const app = express();
 
 const PORT = process.env.NODE_PORT || 3333;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
 
 app.use((req, res, next) => {
   const error = new Error('Not found!');
